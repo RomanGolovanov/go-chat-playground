6c00b0fcef27d3042945cf1069dfae9c2f1a4b0f
Roman Golovanov <roman.golovanov@gmail.com>
2024-07-21 23:21:45 +0100

    remove logging responsewriter override

--- api/middleware/logging.go
+++ api/middleware/logging.go
@@ -6,23 +6,12 @@ import (
 	"time"
 )
 
-type wrappedResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (w *wrappedResponseWriter) WriteHeader(statusCode int) {
-	w.ResponseWriter.WriteHeader(statusCode)
-	w.statusCode = statusCode
-}
-
 func NewLoggingMiddleware() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
-			wrappedWriter := &wrappedResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-			next.ServeHTTP(wrappedWriter, r)
-			log.Println(wrappedWriter.statusCode, r.Method, r.URL.Path, time.Since(start))
+			next.ServeHTTP(w, r)
+			log.Println(r.Method, r.URL.Path, time.Since(start))
 		})
 	}
 }
